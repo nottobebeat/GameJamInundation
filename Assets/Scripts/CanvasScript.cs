901efe5786aab4d8fdaf8db569a2689ff784c5c1
Cody Baldwin <cbald24@WIN2.CS.KSU.EDU>
2017-02-12 01:08:41 -0600

    Game can now be paused and resumed or quit from game screen. Neat! Next: Add music, end of game screen.

---
+++ Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour
+{
+    public GameObject panel;
+
+	void Start ()
+    {
+		
+	}
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            panel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
